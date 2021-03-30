9dba7842f63d3ec31375769577717f756e18c9aa
Higor Luiz <hluiz@id.uff.br>
2021-03-30 15:23:27 -0300

    Adicionando e odificando telas

--- views.py
+++ views.py
@@ -1,6 +1,24 @@
 from flask import render_template, url_for, request, redirect, session, flash
 from app import app
 
+# class higor:
+#     def __init__(self):
+#         self.id = 1
+#         self.remetente = 'fulano@email.com'
+#         self.destinatários=['fulano1@email','fulano21@email']
+#         self.horário = '12:30'
+#         self.assunto= 'Asunto asunto'
+#         self.corpo = 'vamos caminhar no bosque em quanto seu lobo nao vem'
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('tela1.html')
+
+@app.route('/tela2')
+def tela2():
+    nomes = [1,2,3]
+    return render_template('tela2.html',nomes=nomes)
+
+@app.route('/tela3')
+def tela3():
+    return render_template('tela3.html')
